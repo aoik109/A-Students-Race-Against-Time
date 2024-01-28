883a64f35295bdc767fe94da98818c383163ea6f
aoik109 <allison.ko.family@gmail.com>
2024-01-27 22:50:20 -0800

    made bounds for player

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,11 +1,22 @@
 extends Area2D
 
+signal hit_progress
+signal hit_distraction
+signal hit_chore
+
 @export var player_speed = 10
+@export var player_x_coord = 30
+
 const BACKGROUND_WIDTH = 1920
+
+var height_low_bound = 152 # (720-256)-256
+var height_high_bound = 464
+
+var action_type
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	action_type = $AnimatedSprite2D.sprite_frames.get_animation_names()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,3 +25,7 @@ func _process(delta):
 		position.y -= player_speed
 	if Input.is_action_pressed("move_down"):
 		position.y += player_speed
+	position = position.clamp(Vector2(player_x_coord, height_low_bound), Vector2(player_x_coord, height_high_bound))
+
+func _on_body_entered(body):
+	$AnimatedSprite2D.play(action_type[0])
